c34c66333dbaba1cbb87e86e6e03757b66c8f716
unknown <facuugerez37@gmail.com>
2024-04-23 21:13:39 -0300

    edit user form to-do

--- src/pages/admin/usuarios/edit-user.tsx
+++ src/pages/admin/usuarios/edit-user.tsx
@@ -2,7 +2,13 @@ import MainContainer from "../../../components/main/main-container";
 import { Heading } from "../../../components/ui/heading";
 import Layout from "../../../layout/layout";
 import { UserProps } from "../../../../types";
-import { Box, Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 
 const EditUser = () => {
   return (
@@ -13,26 +19,68 @@ const EditUser = () => {
           description={`Edite los datos del usuario.`}
         />
         <Flex
-            borderRadius={5}
-            w='1200px'
-            h='800px'
-            mt={5}
-            position='relative'
-            justifyContent='center'
-            bgColor='gray.300'
+          direction="column" // Establece la dirección de Flexbox como columna
+          alignItems="start" // Alinea los elementos al inicio horizontalmente
+          justifyContent="space-between" // Distribuye el espacio entre los elementos verticalmente
+          borderRadius={5}
+          w="1200px"
+          h="800px"
+          mt={5}
+          position="relative"
+          bgColor="gray.300"
+          boxShadow="md"
+          p={5} // Agrega espacio interno a Flex para los formularios y los botones
         >
           <form>
-            <FormControl>
+            <FormControl gap={5}>
               <FormLabel>Nombre</FormLabel>
-              <Input type="text" />
-              <FormLabel>Apellido</FormLabel>
-              <Input type="text" />
-              <FormLabel>Rol</FormLabel>
-              <Input type="email" />
-              <FormLabel>Correo</FormLabel>
-              <Input colorScheme="teal" type="email" />
+              <Input
+                variant="filled"
+                borderColor="black"
+                focusBorderColor="black"
+                type="text"
+              />
+              <FormLabel mt={4}>Apellido</FormLabel>
+              <Input
+                variant="filled"
+                borderColor="black"
+                focusBorderColor="black"
+                type="text"
+                w={"400px"}
+              />
+              <FormLabel mt={4}>Rol</FormLabel>
+              <Input
+                variant="filled"
+                isReadOnly={true}
+                borderColor="black"
+                focusBorderColor="black"
+                type="text"
+              />
+              <FormLabel mt={4}>Correo</FormLabel>
+              <Input
+                variant="filled"
+                borderColor="black"
+                focusBorderColor="black"
+                type="email"
+              />
             </FormControl>
           </form>
+
+          <Flex justifyContent="flex-end">
+            {" "}
+            {/* Este Flex contiene los botones y los alinea a la derecha */}
+            <Button colorScheme="teal" type="submit">
+              Guardar
+            </Button>
+            <Button
+              colorScheme="red"
+              ml={5}
+              type="submit"
+              onClick={() => window.history.back()}
+            >
+              Cancelar
+            </Button>
+          </Flex>
         </Flex>
       </MainContainer>
     </Layout>
